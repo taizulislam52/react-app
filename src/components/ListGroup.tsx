8c485a60c7ea482a60d067953377df541d3094af
Taizul Islam <taizul@industrydive.com>
2024-07-17 11:42:42 +0600

    Conditional rendering list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -12,7 +12,8 @@ function ListGroup() {
   ];
   return (
     <div className="mt-4 w-50">
-      <h1 className="mb-3">List items</h1>
+      <h1 className="mb-3">Counties name</h1>
+      {countries.length == 0 && <p>Opps! No country found.</p>}
       <ul className="list-group">
         {countries.map((country) => (
           <li key={country} className="list-group-item">
