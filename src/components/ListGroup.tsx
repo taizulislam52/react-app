e34d0f71a6b00689f911f9e5a1b516c29fd845aa
Taizul Islam <taizul@industrydive.com>
2024-07-17 12:27:41 +0600

    Explore basic click event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,7 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
-  const countries = [
+  const countries: string[] = [
     "Bangladesh",
     "Afghanistan",
     "Argentina",
@@ -10,13 +12,16 @@ function ListGroup() {
     "Itely",
     "India",
   ];
+
+  const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
     <div className="mt-4 w-50">
       <h1 className="mb-3">Counties name</h1>
       {countries.length == 0 && <p>Opps! No country found.</p>}
       <ul className="list-group">
         {countries.map((country) => (
-          <li key={country} className="list-group-item">
+          <li key={country} className="list-group-item" onClick={handleClick}>
             {country}
           </li>
         ))}
